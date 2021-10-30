6641c540913ebf733a72d0a047aa4f77f5071cd4
Roberto Ughi <umbe77@gmail.com>
2021-10-31 01:06:11 +0200

    Add first skeleton for main server init

---
+++ yasb.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import winston from 'winston'
+
+const { createLogger, format, transports } = winston
+
+// TODO: Manage logger configuration from file or env variables
+const logger = createLogger({
+  format: format.combine(
+    format.timestamp(),
+    format.simple()
+  ),
+  transports: [
+    new transports.Console({
+      format: format.combine(
+        format.timestamp(),
+        format.colorize(),
+        format.simple()
+      )
+    }),
+  ]
+})
+
+// TODO: Add better support for configuration of port
+const port = process.env.PORT || '8080'
+
+const app = express()
+app.get("/", (_, res) => {
+	res.json({
+		message: 'pippo'
+	})
+	res.end()
+})
+
+app.all("/api", (req, res) => {
+	// TODO: Entry point for all http esb flows
+	res.send("done")
+})
+
+app.listen(port, () => {
+	logger.log({
+		level: 'info',
+		message: `start server listening on port: ${port}`
+	})
+})
