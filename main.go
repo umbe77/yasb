925e3b509fe539e095ea63609bb4b755c7c02482
Roberto Ughi <umbe77@gmail.com>
2022-11-01 21:05:52 +0100

    Initial implementation of Fiber

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	fmt.Println("Hello, world")
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
+
 }
