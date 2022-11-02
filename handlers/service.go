794952e0ee53911b65ac26b2eafe00eb0d364b8d
Roberto Ughi <umbe77@gmail.com>
2022-11-02 18:45:41 +0100

    Add .gitignore and service backbone

---
+++ handlers/service.go
@@ -0,0 +1,9 @@
+package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+// TODO: Implement Sample and example pipeline
+
+func ExecuteService(c *fiber.Ctx) error {
+	return nil
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/umbe77/yasb/handlers"
 )
 
 func main() {
@@ -15,6 +16,8 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	app.All("/service", handlers.ExecuteService)
 
 	log.Fatal(app.Listen(":3000"))
 
