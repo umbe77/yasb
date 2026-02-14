1cc3766ecdfc03bf1e81d809c41cb8271507883a
Roberto Ughi <umbe77@gmail.com>
2026-02-14 15:09:35 +0100

    fix: shadowing configuration module variable

--- config/config.go
+++ config/config.go
@@ -9,16 +9,16 @@ import (
 )
 
 type Config struct {
-	MongoUri string `envconfig:"mongo_db_uri"`
+	MongoUri string `envconfig:"mongodb_uri"`
 }
 
-var (
-	cfg    Config
-	doOnce sync.Once
-)
+var cfg Config
+
+var doOnce sync.Once
 
 func GetConfig() *Config {
 	doOnce.Do(func() {
+		cfg := Config{}
 		err := envconfig.Process("", &cfg)
 		if err != nil {
 			fmt.Printf("Cannot read configuration: %s", err)
